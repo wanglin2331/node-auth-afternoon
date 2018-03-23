a0ceab737c02006e30d379bb9d5518ca0e925fa3
Lin Wang <lin.wang@healthcatalyst.com>
2018-03-22 20:12:12 -0600

    all done

--- index.js
+++ index.js
@@ -1,8 +1,65 @@
 const express = require('express');
 const session = require('express-session');
+require('dotenv').config();
+const passport = require('passport');
+const Auth0Strategy =require('passport-auth0');
+const students = require('./students.json');
+
 
 const app = express();
 
+app.use(session({
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: false
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use( 
+    new Auth0Strategy({
+    domain: process.env.DOMAIN,
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: '/login',
+    scope: 'openid email profile'
+},
+
+function (accessToken, refreshToken, extraParams, profile, done) {
+    return done (null, profile);
+})
+);
+
+passport.serializeUser(function(user,done){
+    done(null, {
+        clientID: user.id,
+        email: user._json.email,
+        name: user._json.name
+    });
+});
+
+passport.deserializeUser(function(obj, done){
+    done(null, obj);
+});
+
+app.get('/login', passport.authenticate('auth0', {
+    successRedirect: '/students',
+    failureRedirect: '/login',
+    connection: 'github'
+}));
+
+const authenticated=(req,res,next)=>{
+    if(req.user){
+        next()
+    } else {
+        res.sendStatus(401)
+    }
+};
+
+app.get('/students',authenticated,(req,res,next)=>{
+        res.status(200).send(students);
+});
 
 const port = 3000;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
